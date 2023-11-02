38d8fbffdaa1b8e72ee9cfbeb4dc99bca9cfe38b
renatopsn <renatopsn@gmail.com>
2023-11-02 14:49:09 -0300

    Adicionando funcionalidade de contacto

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,85 +1,46 @@
-/**
-* PHP Email Form Validation - v3.6
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
-(function () {
-  "use strict";
+$(function () {
+  // init the validator
+  // validator files are included in the download package
+  // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-  let forms = document.querySelectorAll('.php-email-form');
+  $("#contact-form").validator();
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
-      event.preventDefault();
+  // when the form is submitted
+  $("#contact-form").on("submit", function (e) {
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+      var url = "contact.php";
 
-      let thisForm = this;
+      // POST values in the background the the script URL
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $(this).serialize(),
+        success: function (data) {
+          // data = JSON object that contact.php returns
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+          // we recieve the type of the message: success x danger and apply it to the
+          var messageAlert = "alert-" + data.type;
+          var messageText = data.message;
 
-      let formData = new FormData( thisForm );
+          // let's compose Bootstrap alert box HTML
+          var alertBox =
+            '<div class="alert ' +
+            messageAlert +
+            ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+            messageText +
+            "</div>";
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error);
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
-    });
+          // If we have messageAlert and messageText
+          if (messageAlert && messageText) {
+            // inject the alert to .messages div in our form
+            $("#contact-form").find(".messages").html(alertBox);
+            // empty the form
+            $("#contact-form")[0].reset();
+          }
+        },
+      });
+      return false;
+    }
   });
-
-  function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
-  }
-
-  function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
-  }
-
-})();
+});
